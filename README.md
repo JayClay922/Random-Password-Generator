5eafd5790ccc8a56dc1c8dc26596e0da0105e4be
JayClay922 <jamesclayman922@live.com>
2023-01-12 12:59:29 +0000

    adding code to let computer generate and show password based on users preferences

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Random-Password-Generator
+
+## Project
+
+This project required creating an application with which an employee could use ot generate a random password, based on criteria they've selected by modifying a starter code given to us. The application will run in the browser, and will feature dynamically updated HTML and CSS powered by Javascript code that I will write.
+
+## Acceptance Criteria
+
+In creating the application, there are certain criteria that had to be met to ensure that the application fulfilled it's expected behaviour. And they were:
+1. To generate a password when the button is clicked.
+2. Present a series of prompts for password criteria. These password criterias are:
+   - Enter the desired password length(It should be no more than '64 characters' and not less than '10 characters'.
+   - Do you want to include lower case characters?
+   - Do you want to include upper case characters?
+   - Do you want to include numeric characters?
+   - Do you want to include special characters?
+3. The code should validate for each input and at least one character type should be selected.
+4. Once prompts are answered then the password should be generated and displayed in an alert or written to the page.
+
+## Outcome
+
+This challenge was certainly challenging, however I was able to acheive the desired outcome.
+1. I was able to write code that will prompt the user to type in how long they wanted their generated password to be, and if they chose any number outside of the parameters given there would be an alert to remind them that the password should be no more than 64 character and no less than 10 characters
--- js/password-generator.js
+++ js/password-generator.js
@@ -97,15 +97,14 @@ if(chooseYesOrNo === true) {
 }
 
 // Function to prompt user for password options
+let choicesArray = []
+let passwordLength
+
 function getPasswordOptions() {
-   let passwordLength = prompt("Enter the desired password length(It should be no more than '64 characters' and not less than '10 characters'.");
+   passwordLength = prompt("Enter the desired password length(It should be no more than '64 characters' and not less than '10 characters'.");
    if(passwordLength < 10 || passwordLength > 64) {
     alert("Password must be atleast '10 characters' and atmost '64 characters'. Thank you!");
    };
-
-//    var finalPassword = ""
-
-let choicesArray = []
 
 
     let includeSpecialCharacters = confirm("Do you want to include Special Characters?");
@@ -113,68 +112,30 @@ let choicesArray = []
           choicesArray = choicesArray.concat(specialCharacters)
         }
 
-        //   let randomSC = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-        //   let finalSpecialCharacters = ""
-        //     for(let i = 0; i < passwordLength; i++) {
-        //         let randomSC = specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
-        //         finalSpecialCharacters = finalSpecialCharacters + randomSC
-        //     }
-        //     finalPassword = finalPassword + finalSpecialCharacters
-        //     finalPassword = finalPassword.replace(finalPassword[Math.floor(Math.random() * passwordLength)], randomSC)
-        // };
-
     let includeNumericCharacters = confirm("Do you want to include Numeric Characters?");
         if(includeNumericCharacters === true) {
             choicesArray = choicesArray.concat(numericCharacters)
         }
-
-        //     let randomNC = numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
-        //     let finalNumericCharacters = ""
-        //     for(let i = 0; i < passwordLength; i++) {
-        //         let randomNC = numericCharacters[Math.floor(Math.random() * numericCharacters.length)]
-        //         finalNumericCharacters = finalNumericCharacters + randomNC
-        //     }
-        //     finalPassword = finalPassword.replace(finalPassword[3], randomNC)
-        // };
 
     let includeLowerCasedCharacters = confirm("Do you want to include Lower Cased Characters?");
         if(includeLowerCasedCharacters === true) {
             choicesArray = choicesArray.concat(lowerCasedCharacters)
         }
 
-        //     let randomLCC = lowerCasedCharacters[Math.floor(Math.random() * lowerCasedCharacters.length)];
-        //     let finalLowerCase = ""
-        //     for(let i = 0; i < passwordLength; i++) {
-        //         let randomLCC = lowerCasedCharacters[Math.floor(Math.random() * lowerCasedCharacters.length)]
-        //         finalLowerCase = finalLowerCase + randomLCC
-        //     }
-        //     finalPassword = finalPassword.replace(finalPassword[5], randomLCC)
-        // };
-
     let includeUpperCasedCharacters = confirm("Do you want to include Upper Cased Characters?");
         if(includeUpperCasedCharacters === true) {
             choicesArray = choicesArray.concat(upperCasedCharacters)
         }
 
-        //     let randomUCC = upperCasedCharacters[Math.floor(Math.random() * upperCasedCharacters.length)];
-        //     let finalUpperCase = ""
-        //     for(let i = 0; i < passwordLength; i++) {
-        //         let randomUCC = upperCasedCharacters[Math.floor(Math.random() * upperCasedCharacters.length)]
-        //         finalUpperCase = finalUpperCase + randomUCC
-        //     }
-        //     finalPassword = finalPassword.replace(finalPassword[2], randomUCC)
-        // }
-
-}
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-
 }
 
 // Function to generate password with user input
 function generatePassword() {
-    
+    getPasswordOptions()
+    let finalPassword = "";
+    for(let i = 0; i < passwordLength; i++) {
+        finalPassword += choicesArray[Math.floor(Math.random() * choicesArray.length)]
+    } return finalPassword  
 }
 
 // Get references to the #generate element
